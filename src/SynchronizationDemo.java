18ca6598c6412b86146104ce64c78c8311dbbebe
Jerin Prakash <jerinprakash892@gmail.com>
2026-02-19 21:30:53 +0530

    code added

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -3,6 +3,32 @@ class Table {
     // Loop from 1 to 5 (as per sample) or 10
     // Print n * i + " "
     // Handle InterruptedException (try-catch Thread.sleep(400))
+
+	public synchronized void printTable(int n){
+
+		for(int i=1;i<=5;i++){
+
+			System.out.print(n*i);
+			if(i!=n){
+
+				System.out.print(" ");
+
+			}
+			try{
+
+				Thread.sleep(400);
+
+			}catch(InterruptedException e){
+
+				System.out.println(e);
+
+			}
+
+		}
+		System.out.println();
+
+	}
+
 }
 
 class MyThread1 extends Thread {
@@ -12,6 +38,7 @@ class MyThread1 extends Thread {
     }
     public void run() {
         // TODO: Call t.printTable(5)
+	t.printTable(5);
     }
 }
 
@@ -22,6 +49,7 @@ class MyThread2 extends Thread {
     }
     public void run() {
         // TODO: Call t.printTable(100)
+	t.printTable(100);
     }
 }
 
@@ -30,5 +58,10 @@ public class SynchronizationDemo {
         // TODO: Create Table object
         // TODO: Create MyThread1 and MyThread2 objects passing the table object
         // TODO: Start both threads
+	Table tbl = new Table();
+	MyThread1 t1 = new MyThread1(tbl);
+	MyThread2 t2 = new MyThread2(tbl);
+	t1.start();
+	t2.start();
     }
 }
